b3e42703fc6f5543aca94c25d75eb963c0f928c8
facebook <facebook@facebooks-MacBook-Pro.local>
2017-07-21 09:23:05 -0700

    recipe

---
+++ recipebox.py
@@ -0,0 +1,25 @@
+print('GO NUTS FOR PECAN PIE :D')
+print()
+print('list of ingredients you will need to bake a pecan pie')
+ingredients = {}
+
+ingredients = {
+			  "cup of light corn syrup": 1,
+			  "eggs": 3,
+			  "cups of sugar": 1,
+			  "tablespoons melted butter": 2,
+			  "teaspoon of pure vanilla extract": 1,
+			  "cup of pecans": 1,
+			  "pie crust": 1,
+			}
+
+for x in ingredients:
+	print(ingredients[x], x)
+instructions = [' 1.Preheat oven to 350F.', ' 2.Mix corn syrup, eggs, sugar, butter, vanilla, and pecans using a whisk.', ' 3.Pour the filling into the pie crust.', ' 4.Bake on center rack of oven for 60 to 70 minutes.', ' 5.Cool for 2 hours on wire rack before serving.']
+print()
+print('directions:')
+for i in instructions:
+	print(i)
+
+print('ENJOY!!')
+
